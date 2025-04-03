2b04f0273dd156fa44afa636b317e5e3f10529ba
bangdoll <bangdoll@example.com>
2025-04-03 23:01:03 +0800

    fix: 修正 Next.js 配置

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',  // 啟用靜態輸出
-  distDir: 'public', // 設置輸出目錄為 public
   images: {
     unoptimized: true // 靜態輸出需要禁用圖片優化
   }
